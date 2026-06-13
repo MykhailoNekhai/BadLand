70ad70654f51c0eddd34a94a10886edbb6bd3fa4
damir <damirkrupchan@gmail.com>
2026-06-13 20:16:23 +0300

    add component classes (ECS)

---
+++ src/main/java/ua/uni/components/DeadlyComponent.java
@@ -0,0 +1,6 @@
+package ua.uni.components;
+
+import com.badlogic.ashley.core.Component;
+//  Компонент смертельності для головного героя
+public class DeadlyComponent implements Component {
+}
---
+++ src/main/java/ua/uni/components/TextureComponent.java
@@ -0,0 +1,20 @@
+package ua.uni.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Pool;
+
+
+// Компонент який накладає текстуру (спрайт) на об'єкт
+
+public class TextureComponent implements Component, Pool.Poolable {
+    public Texture texture;
+    public float width;
+    public float height;
+    @Override
+    public void reset() {
+        texture = null;
+        width = 0f;
+        height = 0f;
+    }
+}
