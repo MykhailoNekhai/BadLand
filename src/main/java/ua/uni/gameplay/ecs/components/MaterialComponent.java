2b748b95a65deb5ddec43705bd58238ca080223b
damir <damirkrupchan@gmail.com>
2026-06-15 23:53:56 +0300

    add new component

---
+++ src/main/java/ua/uni/gameplay/ecs/components/MaterialComponent.java
@@ -0,0 +1,7 @@
+package ua.uni.gameplay.ecs.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class MaterialComponent implements Component {
+    public String material = "wood"; // default
+}
