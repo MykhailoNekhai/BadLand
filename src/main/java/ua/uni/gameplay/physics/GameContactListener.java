c6636cbca2396d8224486ba1cf7870afdc532228
damir <damirkrupchan@gmail.com>
2026-06-15 23:53:16 +0300

    add new systems

--- src/main/java/ua/uni/gameplay/physics/GameContactListener.java
+++ src/main/java/ua/uni/gameplay/physics/GameContactListener.java
@@ -4,14 +4,29 @@ import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.physics.box2d.*;
 import ua.uni.gameplay.ecs.components.DeadlyComponent;
+import ua.uni.gameplay.ecs.components.FinishComponent;
 import ua.uni.gameplay.ecs.components.PlayerComponent;
 import ua.uni.gameplay.ecs.components.BonusComponent;
+import ua.uni.gameplay.ecs.components.MaterialComponent;
+import ua.uni.gameplay.stats.LevelStats;
+import ua.uni.audio.services.AudioManager;
 
 public class GameContactListener implements ContactListener {
 
     private ComponentMapper<PlayerComponent> playerMapper = ComponentMapper.getFor(PlayerComponent.class);
     private ComponentMapper<DeadlyComponent> deadlyMapper = ComponentMapper.getFor(DeadlyComponent.class);
     private ComponentMapper<BonusComponent> bonusMapper = ComponentMapper.getFor(BonusComponent.class);
+    private ComponentMapper<FinishComponent> finishMapper = ComponentMapper.getFor(FinishComponent.class);
+    private ComponentMapper<MaterialComponent> materialMapper = ComponentMapper.getFor(MaterialComponent.class);
+    private LevelStats levelStats;
+
+    public GameContactListener(LevelStats levelStats) {
+        this.levelStats = levelStats;
+    }
+
+    public GameContactListener() {
+        this(null);
+    }
 
     @Override
     public void beginContact(Contact contact) {
@@ -27,6 +42,9 @@ public class GameContactListener implements ContactListener {
 
         checkBonus(objA, objB);
         checkBonus(objB, objA);
+
+        checkFinish(objA, objB);
+        checkFinish(objB, objA);
     }
 
     private void checkDeath(Object supposedPlayer, Object supposedDeadly) {
@@ -36,6 +54,9 @@ public class GameContactListener implements ContactListener {
 
             if (playerMapper.has(playerEntity) && deadlyMapper.has(deadlyEntity)) {
                 playerMapper.get(playerEntity).isDead = true;
+
+
+
             }
         }
     }
@@ -52,7 +73,36 @@ public class GameContactListener implements ContactListener {
                 if (!bonus.isCollected) {
                     player.receivedBonus = bonus.type;
                     bonus.isCollected = true;
+                    
+                    if ("item-clone".equals(bonus.type)) {
+                        AudioManager.get().playRandomCloneSound(1.0f);
+                        if (levelStats != null) {
+                            levelStats.collectedClones++;
+                        }
+                    } else if ("item-big".equals(bonus.type)) {
+                        AudioManager.get().playBiggerSound(1.0f);
+                    } else if ("item-small".equals(bonus.type)) {
+                        AudioManager.get().playSmallerSound(1.0f);
+                    } else if ("item-slow".equals(bonus.type)) {
+                        AudioManager.get().playSlowerSound(1.0f);
+                    } else if ("item-speed".equals(bonus.type)) {
+                        AudioManager.get().playFastSound(1.0f);
+                    }
                 }
+            }
+        }
+    }
+
+    private void checkFinish(Object supposedPlayer, Object supposedFinish) {
+        if (supposedPlayer instanceof Entity && supposedFinish instanceof Entity) {
+            Entity playerEntity = (Entity) supposedPlayer;
+            Entity finishEntity = (Entity) supposedFinish;
+
+            if (playerMapper.has(playerEntity) && finishMapper.has(finishEntity)) {
+                if (levelStats != null) {
+                    levelStats.levelFinished = true;
+                }
+                playerMapper.get(playerEntity).isFinished = true;
             }
         }
     }
@@ -72,16 +122,53 @@ public class GameContactListener implements ContactListener {
 
             boolean isPlayerA = playerMapper.has(entityA);
             boolean isBonusB = bonusMapper.has(entityB);
+            boolean isFinishB = finishMapper.has(entityB);
             boolean isPlayerB = playerMapper.has(entityB);
             boolean isBonusA = bonusMapper.has(entityA);
+            boolean isFinishA = finishMapper.has(entityA);
 
             if ((isPlayerA && isBonusB) || (isPlayerB && isBonusA)) {
                 contact.setEnabled(false);
+            }
+            if ((isPlayerA && isFinishB) || (isPlayerB && isFinishA)) {
+                contact.setEnabled(false); // Make the finish line act as a sensor
             }
         }
     }
 
     @Override
     public void postSolve(Contact contact, ContactImpulse impulse) {
+        Object objA = contact.getFixtureA().getBody().getUserData();
+        Object objB = contact.getFixtureB().getBody().getUserData();
+
+        if (objA instanceof Entity && objB instanceof Entity) {
+            Entity entityA = (Entity) objA;
+            Entity entityB = (Entity) objB;
+
+            boolean isPlayerA = playerMapper.has(entityA);
+            boolean isPlayerB = playerMapper.has(entityB);
+
+            if (isPlayerA || isPlayerB) {
+                float maxImpulse = 0f;
+                float[] normalImpulses = impulse.getNormalImpulses();
+                for (float imp : normalImpulses) {
+                    if (imp > maxImpulse) {
+                        maxImpulse = imp;
+                    }
+                }
+                
+                // Порог, чтобы не издавать звук при простом качении по полу
+                if (maxImpulse > 2.0f) {
+                    String material = "rubber"; // по умолчанию звук резины (или тела)
+                    if (isPlayerA && !isPlayerB && materialMapper.has(entityB)) {
+                        material = materialMapper.get(entityB).material;
+                    } else if (isPlayerB && !isPlayerA && materialMapper.has(entityA)) {
+                        material = materialMapper.get(entityA).material;
+                    }
+
+                    AudioManager.get().playImpactSound(maxImpulse, material);
+                }
+            }
+        }
     }
 }
